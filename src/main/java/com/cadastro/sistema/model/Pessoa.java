8d5b69c50b4a2eba19327eb1cb90c9e09cdfbe6b
Diego Munhoz <jose.francelino@7comm.com.br>
2026-03-27 15:19:39 -0300

    feat: inicialização do projeto

---
+++ src/main/java/com/cadastro/sistema/model/Pessoa.java
@@ -0,0 +1,16 @@
+package com.cadastro.sistema.model;
+
+public class Pessoa {
+
+    private String nome;
+    private String cpf;
+    private String data;
+    private String email;
+    private String cep;
+    private String rua;
+    private int numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+
+}
